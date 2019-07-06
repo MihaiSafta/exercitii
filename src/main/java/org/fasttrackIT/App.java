23fd23ec66dae3da18d89721c94b70c34cda4cb0
Mihai <mihaisaftas.259@gmail.com>
2019-07-06 14:32:21 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackIT/App.java
@@ -0,0 +1,30 @@
+package org.fasttrackIT;
+
+
+public class App {
+
+    //public void displayAverage(double a,double b,double c)
+//        {
+//            System.out.println("The numbers are :" + a + "," + b + "," + c);
+//
+//                double total=(a+b+c);
+//                System.out.println("Total is : " + total);
+//
+//                double average=(a+b+c) / 3;
+//                System.out.println("Average = " + average);
+//
+//        }
+//
+//
+//public static void main(String[]args)
+//        {
+//        App app = new App();
+//        app.displayAverage(3.5,4.5,5);
+//        }
+    public static void main(String[] args) {
+
+
+        LogicalOp exercise1 = new LogicalOp();
+        exercise1.numbercounting(100);
+    }
+}
---
+++ src/main/java/org/fasttrackIT/LogicalOp.java
@@ -0,0 +1,81 @@
+package org.fasttrackIT;
+
+
+
+public class LogicalOp {
+    // Ex nr.1
+//    public void numbercounting(int startingNumber) {
+//        for (int i = startingNumber; i <= 100; i++) {
+//            System.out.println(i);}}
+
+
+    //  Ex nr.2
+//    public void numbercounting(int startingNumber) {
+//        for (int i = startingNumber; i >= -100; i--) {
+//            System.out.println(i);}}
+
+
+    //Ex nr.3
+//    public void numbercounting(int startingNumber, int endingNumber) {
+//        for (int i = startingNumber; i <= endingNumber; i++) {
+//            System.out.println(i);
+//        }
+//    }
+
+//    Ex. nr.4
+//public void numbercounting(int x, int y){
+//   if(x<y){
+//    for (int i = x; i <= y; i++){
+//    System.out.println(i);
+//}}
+//   else if(x>y){for (int i =y; i <= x; i++)
+//       System.out.println(i);}
+//}
+
+        //Ex nr.4
+//    public void numbercounting(){
+//          for(int i = 1; i<=100; i++){
+//    if(i % 2 == 0){
+//    System.out.println(i);}}}
+
+//    Ex nr.5
+//    public void numbercounting(){
+//        for(int i = 1; i<=100; i++){
+//            if(i % 2 != 0){
+//                System.out.println(i);}}}
+
+//    Ex. nr.6
+//    public void numbercounting(int sum){
+//
+//        int num = 100;
+//        for(int i = sum; i <= num; i++)
+//        {
+//            sum += i;
+//        }
+//        System.out.println("Sum = " + sum);
+//    }
+
+public void numbercounting(int givenNumer) {
+    for (int i = givenNumer; i <= 100; i++) {
+         givenNumer += i;}
+
+        System.out.println("Sum = " + givenNumer);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
